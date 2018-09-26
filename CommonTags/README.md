2d0f2746e70c945753962d1b8b6c16f3703c05b0
Jimmy Dahlqvist <jimmy.dahlqvist@infor.com>
2018-09-26 18:31:47 +0200

    Add CommonTags macro

---
+++ CommonTags/README.md
@@ -0,0 +1,88 @@
+# Macro RequiredTags
+  
+What is the problem that we try to solve with this macro?  
+It's really common that you want to add the same tag to all of your resources, it could be for cost allocation or similar.  
+Adding the same tag value over and over again produce a lot of boiler plate code. This macro will add specified common tags to all resources supporting tags.  
+
+## The Macro
+
+This Macro will add tags specified in the parameter section to all resources that support tags.  
+The macro is intended to be run on the entire template and should be put in the "Transform" section.  
+If a tag-key is already present on a resources this macro will not modify the value. That mean that you can override the default values.  
+
+## Parameters
+
+The parameters should be the tags to be added as a key:value pair.  
+
+```yaml
+  Parameters:
+    'MyKey' : 'MyValue'
+    'MyKey2' : 'MyValue2'
+```  
+
+You can override the default value of a key by just specifying it on your resource as well.  
+
+```yaml
+  Bucket1:
+    Type: AWS::S3::Bucket
+    Properties:
+      BucketName: my-super-duper-testing-bucket
+      Tags:
+        - Key: 'MyKey'
+          Value: 'this overrides the default value for MyKey in the macro parameters'
+```  
+
+## Deploying the Macro
+
+The macro is deployed by deploying the CloudFormation template macro.yaml  
+Below is an example on how to do that.
+
+```bash
+#!/bin/bash
+
+DEPLOYMENT_BUCKET_NAME=my-temporary-deploy-bucket
+
+echo "creating package ..."
+aws cloudformation package \
+    --template-file macro.yaml \
+    --s3-bucket ${DEPLOYMENT_BUCKET_NAME} \
+    --output-template-file packaged-template.yaml
+
+echo "deploying package ..."
+aws cloudformation deploy \
+    --template-file packaged-template.yaml \
+    --stack-name cfn-macro-common-tags\
+    --capabilities CAPABILITY_NAMED_IAM
+ ```  
+  
+## Example usage
+
+```yaml
+AWSTemplateFormatVersion: '2010-09-09'
+Description: CommonTags macro example template
+Transform:
+  Name: CommonTags
+  Parameters:
+    'Name' : 'MySuperCoolApp'
+    'CreatedBy' : 'Me'
+    'CostAllocation' : 'MySuperCoolApp'
+
+Resources:
+  Bucket1:
+    Type: AWS::S3::Bucket
+    Properties:
+      BucketName: my-super-duper-cool-app-bucket
+      Tags:
+        - Key: CostAllocation
+          Value: override
+  
+  EC2Instance:
+    Type: AWS::EC2::Instance
+    Properties:
+      InstanceType: t2.micro
+      ImageId: ami-123456789
+      SubnetId: subnet-123456789
+      SecurityGroupIds:
+        - sg-123456789
+      Monitoring: False
+```  
---
+++ CommonTags/src/index.py
@@ -0,0 +1,105 @@
+# The MIT License (MIT)
+#
+# Copyright (C) 2018 Jimmy Dahlqvist
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+# IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import json
+import logging
+
+log = logging.getLogger('Macro-CommonTags')
+log.setLevel(logging.DEBUG)
+
+# Load data about resources supporting tags.
+# Do it outside of the handler to enable resuse.
+ROSOURCES_SUPPORTING_TAGS = []
+with open('resources_supporting_tags.json') as f:
+    ROSOURCES_SUPPORTING_TAGS = json.load(f)
+
+
+def handler(event, context):
+    log.debug(json.dumps(event, indent=2))
+
+    tags = get_parameters(event)
+    if not tags:
+        return create_failure(event)
+
+    resources = get_resources(event)
+    if resources == None:
+        return create_failure(event)
+
+    tag_resources(resources, tags)
+
+    return {
+        'requestId': event['requestId'],
+        'status': 'success',
+        'fragment': event['fragment']
+    }
+
+
+def tag_resources(resources, tags):
+    for resource_key in resources:
+        add_tags_to_resource(resources[resource_key], tags)
+
+
+def add_tags_to_resource(resource, tags):
+    if does_resource_support_tags(resource):
+        resource_tags = resource['Properties'].get('Tags', [])
+        log.debug(json.dumps(resource_tags, indent=2))
+        for tag in tags:
+            if not is_tag_already_specified(tag, resource_tags):
+                tag_object = {
+                    'Key': tag,
+                    'Value': tags[tag]
+                }
+                resource_tags.append(tag_object)
+        resource['Properties']['Tags'] = resource_tags
+        log.debug(json.dumps(resource_tags, indent=2))
+
+
+def is_tag_already_specified(tag, resource_tags):
+    for tag_object in resource_tags:
+        if tag_object['Key'] == tag:
+            return True
+    return False
+
+
+# Check if a resource support Tags, since not all resources does that.
+def does_resource_support_tags(resource):
+    if 'Tags' in resource['Properties']:
+        return True
+
+    if resource['Type'] in ROSOURCES_SUPPORTING_TAGS:
+        return True
+
+    return False
+
+
+def get_parameters(event):
+    if 'params' in event:
+        return event['params']
+    return {}
+
+
+def get_resources(event):
+    if 'Resources' in event['fragment']:
+        return event['fragment']['Resources']
+    return None
+
+
+def create_failure(event):
+    return {
+        'requestId': event['requestId'],
+        'status': 'failed',
+        'fragment': event['fragment']
+    }
